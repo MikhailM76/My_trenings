8a9bdd937ef3206bd01b2c29012380aa5ce685b0
MikhailM76 <mmmsl1976@gmail.com>
2024-11-16 14:12:13 +0300

    Add files via upload

---
+++ Peremennye.py
@@ -0,0 +1,4 @@
+vypolneno_dz=12
+zatracheno_chasov=1.5
+nazv_kursa="Python"
+print("Курс:",nazv_kursa,", выполнено задач:",vypolneno_dz,", затрачено часов:",zatracheno_chasov,", в среднем на одну задачу:", zatracheno_chasov/vypolneno_dz)
