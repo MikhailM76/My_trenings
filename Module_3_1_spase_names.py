a50931a6b05afdd5e0640663d4900ccf1bee47ea
MikhailM76 <mmmsl1976@gmail.com>
2024-12-18 21:55:28 +0300

    Add files via upload

--- Module_3_1_spase_names.py
+++ Module_3_1_spase_names.py
@@ -1,20 +1,35 @@
 calls = 0
+
+
 def count_calls():
-    pass
-def string_info(string):
-    count_calls()
-    return len(string), "string".upper,"string".lower
+    global calls
+    calls += 1
+    return calls
+count_calls()
 
-def is_contains(string, list_to_search):
-    if string in list_to_search:
-        is_contains == True
-    else:
-        is_contains == False
-
-    count_calls()
-print(is_contains)
-print(string_info)
-print(count_calls)
+calls = count_calls()
 print(calls)
+
+
+def string_info(string):
+    string_1= len (string)
+    string_2 = string.upper()
+    string_3= string.lower()
+    return string_1, string_2, string_3
+    #count_calls()
+str_=string_info("КраКозябра")
+print(str_)
+
+# def is_contains(string, list_to_search):
+#     if string in list_to_search:
+#         return
+# is_str = is_contains('Колушата',['бутявка', 'Колушата'] )
+# print(is_str)
+
+#     count_calls()
+#print(is_contains)
+# print(string_info)
+# print(count_calls)
+# print(calls)
 
 
